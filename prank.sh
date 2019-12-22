2a552cb9955e8a12b48ea1bba7011d9e8edd99f5
MrBedrocks <59139180+MrBedrocks@users.noreply.github.com>
2019-12-22 20:47:20 +0700

    Add files via upload

---
+++ prank.sh
@@ -0,0 +1,101 @@
+figlet HACK SYSTEM|lolcat
+figlet ANDROID/IOS|lolcat
+sleep 1
+sleep 1
+sleep 1
+echo "------------------------|"
+echo "1.xiomi                 |"
+echo "2.oppo                  |"
+echo "3.samsung               |"
+echo "4.iphone                |"
+echo "5.ipad                  |"
+echo "6.realme                |"
+echo "7.vivo                  |"
+echo "8.asus                  |"
+echo "9.rog                   |"
+echo "10.black berry          |"
+echo "________________________|"
+sleep 1
+read -p "pilih nama hp": bos;
+sleep 1
+sleep 1
+read -p "type handphone:" bos;
+sleep 1
+sleep 1
+figlet loading..|lolcat
+sleep 1
+sleep 1 
+sleep 1
+read -p "nomer imei:" bos;
+read -p "alamat ip:" bos;
+sleep 1
+sleep 1
+figlet Loading..|lolcat
+sleep 1
+sleep 1
+sleep 1
+sleep 1
+sleep 1
+sleep 1
+sleep 1
+sleep 5
+sleep 5
+figlet loading..| lolcat
+figlet 10%|lolcat
+figlet 25%|lolcat
+figlet 30%|lolcat
+figlet 40%|lolcat
+sleep 1
+sleep 1
+figlet 45%|lolcat
+sleep 1
+sleep 1
+sleep 1
+sleep 1
+figlet 50%|lolcat
+sleep 1
+sleep 1
+figlet 65%|lolcat
+sleep 1
+figlet 70%|lolcat
+sleep 1 
+sleep 1
+sleep 1
+sleep 1
+sleep 1
+sleep 1
+sleep 1
+sleep 1
+sleep 1
+sleep 5
+figlet 80%|lolcat
+sleep 5
+sleep 5
+sleep 5
+figlet 91%|lolcat
+sleep 1
+sleep 1
+sleep 1
+sleep 1
+sleep 5
+sleep 1
+sleep 5
+sleep 1
+sleep 5
+sleep 1
+figlet 99%|lolcat
+sleep 5
+figlet 100%|lolcat
+sleep 1
+sleep 1
+sleep 5
+figlet Loading..|lolcat
+sleep 5
+sleep 5
+sleep 5
+sleep 5
+sleep 5
+sleep 1
+sleep 1
+sleep 1
+figlet error 402|lolcat
